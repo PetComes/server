b22b4ccdd1a8724852a1ae4d6355be1d728455e2
TonyJHC <jongvin01@naver.com>
2021-11-05 02:06:01 +0900

    Entity 정의

---
+++ src/main/java/com/pet/comes/model/Entity/Family.java
@@ -0,0 +1,31 @@
+package com.pet.comes.model.Entity;
+
+
+import com.pet.comes.model.Timestamped;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Time;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@IdClass(Family.class) // PK 2개일때
+public class Family extends Timestamped implements Serializable { // @Idclass 있을 시 Serializable 인터페이스 구현
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long familyId;
+
+    private int isUser; // 가족 id 가 유저이면 1, 강아지면 0
+
+
+
+
+}
