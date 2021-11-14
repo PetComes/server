f86ab385d5b1d1313e6b0351b05b0f0a6fceedac
TonyJHC <jongvin01@naver.com>
2021-11-15 02:14:59 +0900

    홈-상단부-1 [H4] POST 강아지 등록 API

--- src/main/java/com/pet/comes/model/Entity/Family.java
+++ src/main/java/com/pet/comes/model/Entity/Family.java
@@ -1,11 +1,16 @@
 package com.pet.comes.model.Entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Family {
 
     @Id
@@ -13,10 +18,16 @@ public class Family {
     @Column(name = "family_id")
     private Long id;
 
-    private Long dogId;
+//    private Long dogId;
 
     @OneToMany(mappedBy = "family")
     private List<Dog> dogs = new ArrayList<Dog>();
 
+    public void setDogs(Dog dog) {
+        dogs.add(dog);
+    }
+
     private LocalDateTime createdAt;
+
+
 }
--- src/main/java/com/pet/comes/service/DogService.java
+++ src/main/java/com/pet/comes/service/DogService.java
@@ -2,7 +2,11 @@ package com.pet.comes.service;
 
 import com.pet.comes.dto.Req.DogReqDto;
 import com.pet.comes.model.Entity.Dog;
+import com.pet.comes.model.Entity.Family;
+import com.pet.comes.model.Entity.User;
 import com.pet.comes.repository.DogRepository;
+import com.pet.comes.repository.FamilyRepository;
+import com.pet.comes.repository.UserRepository;
 import com.pet.comes.response.DataResponse;
 import com.pet.comes.response.NoDataResponse;
 import com.pet.comes.response.ResponseMessage;
@@ -13,16 +17,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 
+
 @Service
 public class DogService {
 
     private final DogRepository dogRepository;
     private final Status status;
     private final ResponseMessage message;
+    private final FamilyRepository familyRepository;
+    private final UserService userService;
 
     @Autowired
-    public DogService(DogRepository dogRepository, Status status, ResponseMessage message){
+    public DogService(UserService userService, FamilyRepository familyRepository,DogRepository dogRepository,Status status, ResponseMessage message){
         this.dogRepository = dogRepository;
+        this.familyRepository = familyRepository;
+        this.userService =userService;
         this.status = status;
         this.message = message;
     }
@@ -30,19 +39,23 @@ public class DogService {
 
 
     public ResponseEntity addDog(Long userId,DogReqDto dogReqDto ){
-        if(dogReqDto.getName() == null  || dogReqDto.getAge() > 20 ){
+        if(dogReqDto.getName() == null  || dogReqDto.getAge() > 25 ){
+
             return new ResponseEntity(NoDataResponse.response(status.NOT_ENTERED, message.NOT_ENTERED), HttpStatus.OK);
 
         }
 
-        Dog dog = new Dog(dogReqDto);
-        //Optional<Dog>
+
+        Family family = new Family(); // dog 생성시 family 객체 생성
+
+        Dog dog =  new Dog(dogReqDto,family); // dog -> family 관계 매핑
+        family.setDogs(dog); // family -> dog 관계 매핑
+
+        familyRepository.save(family); // dog <-> family 관계 매핑 끝난 family 객체 DB 반영
+        userService.setFamilyId(userId, family); // User -> Family 관계 매핑
+
 
         dogRepository.save(dog);
-
-//        FamilyDto familyDto = new FamilyDto(userId,dog.getId(),0);
-
-//        Family family = new Family(familyDto);
 
 
 
