1c6ca9e44680e5100b0462a0017a35a2d0d70839
TonyJHC <jongvin01@naver.com>
2021-11-17 02:30:54 +0900

    .

--- src/main/java/com/pet/comes/controller/DiaryController.java
+++ src/main/java/com/pet/comes/controller/DiaryController.java
@@ -1,10 +1,7 @@
 package com.pet.comes.controller;
 
 
-import com.pet.comes.repository.DiaryRepository;
 import com.pet.comes.service.DiaryService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
