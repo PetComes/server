0d47d31a9d77b856e3ac847df2038a095dc417cb
HanInJu <haninju95@gmail.com>
2022-02-23 20:44:26 +0900

    [Snack] 엔티티 수정

--- src/main/java/com/pet/comes/model/Entity/schedule/Snack.java
+++ src/main/java/com/pet/comes/model/Entity/schedule/Snack.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -33,9 +34,11 @@ public class Snack {
 
 	private LocalDate date; // YYYY-MM-DD
 	private LocalTime time; // HH:mm:ss
+
+	@Column(columnDefinition = "TEXT")
 	private String memo;    // 자유메모
 
-	private String kind;
+	private String kind; 	// 간식종류 VARCHAR(255)
 
 	private LocalDateTime modifiedAt;
 	private LocalDateTime registeredAt;
