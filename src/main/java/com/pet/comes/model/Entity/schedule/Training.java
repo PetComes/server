502c8c8e1df6b0cebec314ce0f1b1bf4e61da8a2
HanInJu <haninju95@gmail.com>
2022-02-23 20:44:59 +0900

    [Training] 엔티티 수정

--- src/main/java/com/pet/comes/model/Entity/schedule/Training.java
+++ src/main/java/com/pet/comes/model/Entity/schedule/Training.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -28,6 +29,8 @@ public class Training {
 
 	private LocalDate date; // YYYY-MM-DD
 	private LocalTime time; // HH:mm:ss
+
+	@Column(columnDefinition = "TEXT")
 	private String memo;    // 자유메모
 
 	private LocalDateTime modifiedAt;
