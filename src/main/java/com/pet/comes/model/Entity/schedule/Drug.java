f9c73bf612fef9c3e1ed35a7ecf1a4e33395e426
HanInJu <haninju95@gmail.com>
2022-02-23 20:36:49 +0900

    [Drug] 엔티티 수정

--- src/main/java/com/pet/comes/model/Entity/schedule/Drug.java
+++ src/main/java/com/pet/comes/model/Entity/schedule/Drug.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -28,10 +29,14 @@ public class Drug {
 
 	private LocalDate date; // YYYY-MM-DD
 	private LocalTime time; // HH:mm:ss
+
+	@Column(columnDefinition = "TEXT")
 	private String memo;    // 자유메모
 
-	private String kind;
-	private String prescriptionUrl;
+	private String kind; // 약 종류 VARCHAR(255)
+
+	@Column(columnDefinition = "TEXT")
+	private String prescriptionUrl; // 처방전 URL
 	private int expenses;
 
 	private LocalDateTime modifiedAt;
